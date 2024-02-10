67fe0ca70c14183e666c96e6ae4ac9254c2382ee
Bisrat Kebere <kiyyakebere@gmail.com>
2024-02-11 02:30:12 +0900

    Refactor error handling functions and include line and column information

--- headers/error.h
+++ headers/error.h
@@ -5,25 +5,10 @@
 #ifndef ERROR_H
 #define ERROR_H
 
-void handleError(const char* msg, const char* file, int line, bool abort);
-void handleError(const char* msg, int line, bool abort);
-void handleError(const char* msg, bool abort);
-void handleError(const char* msg);
-
-void handleWarning(const char* msg, const char* file, int line);
-void handleWarning(const char* msg, int line);
-void handleWarning(const char* msg, bool abort);
-void handleWarning(const char* msg);
-
-void handleError(const string msg, const string file, int line, bool abort);
-void handleError(const string msg, int line, bool abort);
-void handleError(const string msg, bool abort);
-void handleError(const string msg);
-
-void handleWarning(const string msg, const string file, int line);
-void handleWarning(const string msg, int line);
-void handleWarning(const string msg, bool abort);
-void handleWarning(const string msg);
+void handleError(const string msg, int line, int column, bool abort);
+void handleError(const string msg, int line, int column);
+void handleWarning(const string msg, int line, int column, bool abort);
+void handleWarning(const string msg, int line, int column);
 
 
 #endif // ERROR_H
--- src/error.cpp
+++ src/error.cpp
@@ -1,110 +1,27 @@
 #include <iostream>
+#include <iomanip>
 #include "../headers/error.h"
 
 using namespace std;
 
-void handleError(const char* msg, const char* file, int line, bool abort = true) {
+void handleError(const string msg, int line, int column, bool abort) {
     cerr << "\033[1;31m";
-    cerr << "Error: " << msg << " in " << file << " at line " << line << endl;
+    cerr << line << ":" << column << ": Error: " << msg << endl;
     cerr << "\033[0m";
     if (abort) exit(EXIT_FAILURE);
 }
 
-void handleError(const char* msg, int line, bool abort = true) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << " at line " << line << endl;
+void handleError(const string msg, int line, int column) {
+    handleError(msg, line, column, true);
+}
+
+void handleWarning(const string msg, int line, int column, bool abort) {
+    cerr << "\033[1;33m";
+    cerr << line << ":" << column << ": Warning: " << msg << endl;
     cerr << "\033[0m";
     if (abort) exit(EXIT_FAILURE);
 }
 
-void handleError(const char* msg) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << endl;
-    cerr << "\033[0m";
-    exit(EXIT_FAILURE);
-}
-
-void handleError(const char* msg, bool abort) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << endl;
-    cerr << "\033[0m";
-    if (abort) exit(EXIT_FAILURE);
-}
-
-void handleWarning(const char* msg, const char* file, int line) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << " in " << file << " at line " << line << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const char* msg, int line) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << " at line " << line << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const char* msg) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const char* msg, bool abort) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << endl;
-    cerr << "\033[0m";
-    if (abort) exit(EXIT_FAILURE);
-}
-
-void handleError(const string msg, const string file, int line, bool abort = true) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << " in " << file << " at line " << line << endl;
-    cerr << "\033[0m";
-    if (abort) exit(EXIT_FAILURE);
-}
-
-void handleError(const string msg, int line, bool abort = true) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << " at line " << line << endl;
-    cerr << "\033[0m";
-    if (abort) exit(EXIT_FAILURE);
-}
-
-void handleError(const string msg) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << endl;
-    cerr << "\033[0m";
-    exit(EXIT_FAILURE);
-}
-
-void handleError(const string msg, bool abort) {
-    cerr << "\033[1;31m";
-    cerr << "Error: " << msg << endl;
-    cerr << "\033[0m";
-    if (abort) exit(EXIT_FAILURE);
-}
-
-void handleWarning(const string msg, const string file, int line) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << " in " << file << " at line " << line << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const string msg, int line) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << " at line " << line << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const string msg) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << endl;
-    cerr << "\033[0m";
-}
-
-void handleWarning(const string msg, bool abort) {
-    cerr << "\033[1;33m";
-    cerr << "Warning: " << msg << endl;
-    cerr << "\033[0m";
-    // if (abort) exit(EXIT_FAILURE);
+void handleWarning(const string msg, int line, int column) {
+    handleWarning(msg, line, column, false);
 }
